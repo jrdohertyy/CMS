5ff918ddc7929ffbbf595a75a003154b1e1bd06f
JOHNROSSDOHERTY-STUD <g00423043@atu.ie>
2024-11-04 09:11:17 +0000

    Customer class & constructor

---
+++ src/main/java/ie/atu/cms/Customer.java
@@ -0,0 +1,16 @@
+package ie.atu.cms;
+
+public class Customer {
+    String customerID;
+    String name;
+    String email;
+    String phoneNumber;
+
+    //Constructor for customer class
+    Customer(String customerID, String name, String email, String phoneNumber){
+        this.customerID = customerID;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
