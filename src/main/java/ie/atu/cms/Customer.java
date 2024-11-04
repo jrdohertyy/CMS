01c3760f4d2817408402289cd5ddf8ab211269a0
JOHNROSSDOHERTY-STUD <g00423043@atu.ie>
2024-11-04 09:16:55 +0000

    Added getter&setter methods

--- src/main/java/ie/atu/cms/Customer.java
+++ src/main/java/ie/atu/cms/Customer.java
@@ -13,4 +13,34 @@ public class Customer {
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
+
+    //Example of a getter & setter method for customer class and fields
+    public String getcustomerID(){
+        return customerID;
+    }
+    public void setcustomerID(String customerID){
+        this.customerID = customerID;
+    }
+
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+
+    public String getemail(){
+        return email;
+    }
+    public void setemail(String email){
+        this.email = email;
+    }
+
+    public String getphoneNumber(){
+        return phoneNumber;
+    }
+    public void setphoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
 }
