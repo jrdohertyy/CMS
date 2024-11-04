f16f5d65db6d06b12dfdf616cb0946131622bab7
JOHNROSSDOHERTY-STUD <g00423043@atu.ie>
2024-11-04 09:27:37 +0000

    Trying to fix git issue

--- src/main/java/ie/atu/cms/Customer.java
+++ src/main/java/ie/atu/cms/Customer.java
@@ -13,4 +13,36 @@ public class Customer {
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
+
+    public String getcustomerID(){
+        return customerID = customerID;
+    }
+
+    public void setcustomerID(String customerID){
+        this.customerID = customerID;
+    }
+
+    public String getname(){
+        return name = name;
+    }
+
+    public void setname(String name){
+        this.name = name;
+    }
+
+    public String getemail(){
+        return email = email;
+    }
+
+    public void setemail(String email){
+        this.email = email;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber = phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
 }
